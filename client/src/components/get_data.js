8cbc5e0360ccd51aa222e1db0994ef885be852ab
camposss <ccamposs23@gmail.com>
2017-12-22 12:36:07 -0800

    finished react setup

---
+++ client/src/components/get_data.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class GetData extends Component {
+    render(){
+        return(
+            <div>
+                <h1>This is Get Data</h1>
+            </div>
+        )
+    }
+}
+export default GetData;
---
+++ client/src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => {
+    return(
+        <div>
+            <h1>This is the Home Page</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa dignissimos excepturi illum officiis, sint totam vero! Aperiam atque debitis dolorem ea illum iusto labore maiores, neque quisquam, quo quos tempore!</p>
+        </div>
+    )
+}
